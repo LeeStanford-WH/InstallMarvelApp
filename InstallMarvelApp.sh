1e4fc2641f9f56a5d156f7fc7f61536d0af234a9
Lee Stanford <lee.stanford@workhuman.com>
2019-04-03 08:18:12 -0400

    Added code comments

--- InstallMarvelApp.sh
+++ InstallMarvelApp.sh
@@ -4,17 +4,22 @@
 cd /Applications
 osascript -e 'quit app "Marvel"'
 sudo rm -rf Marvel.app
-#
-#
+
+# Create temp directory for the download
 mkdir /private/var/tmp/Marvel
 cd /private/var/tmp/Marvel
 
-# Installing Coda
+# Download and unzip Marvel.app
 curl -L -O "https://storage.googleapis.com/sketch-plugin/Marvel.zip"
 unzip "Marvel.zip"
+
+# Copy the downloaded app to the Applications folder
 sudo mv Marvel.app /Applications
+
+# Delete the temp folder
 cd /private/var
 hdiutil detach /Volumes/Google\ Chrome
 /bin/rm -rf /private/var/tmp/chrome
+
 exit 0      ## Success
 exit 1      ## Failure
